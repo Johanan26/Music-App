25af649cc51269632bb1b2c5503556b00a1f9cf9
G00420811@atu.ie <g00420811@atu.ie>
2024-04-11 11:01:11 +0100

    Added test package for Interface

---
+++ src/main/java/ie/atu/jdbc/test_interface/App.java
@@ -0,0 +1,20 @@
+package ie.atu.jdbc.test_interface;
+
+import java.util.ArrayList;
+public class App {
+    public static void main(String[] args) {
+// array of Music objects
+        Music[] musicArray = new Music[]{
+// Music items
+                new Songs("What", "test"),
+                new Songs("hello", "test"),
+// add more music items here if needed
+        };
+// Displaying details
+        for (Music music : musicArray) {
+// printing out the name and artist for each music item
+            System.out.println("\nSong Name: " + music.getName());
+            System.out.println("Artist: " + music.getArtist());
+        }
+    }
+}
